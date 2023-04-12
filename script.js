9a57b91927dd3fbb402895ddcf51efe1f19a4768
Brandon Ashby <bashby@corp.justiceworks.com>
2023-04-12 08:16:44 -0600

    added remaining criteria

--- script.js
+++ script.js
@@ -1,45 +1,64 @@
 function getRadioButtonValue(form, radioButtonGroupName) {
-    const radioButtons = form.elements[radioButtonGroupName];
-    for (let i = 0; i < radioButtons.length; i++) {
-        if (radioButtons[i].checked) {
-            return parseInt(radioButtons[i].value);
-        }
+  const radioButtons = form.elements[radioButtonGroupName];
+  for (let i = 0; i < radioButtons.length; i++) {
+    if (radioButtons[i].checked) {
+      return parseInt(radioButtons[i].value);
     }
-    return 0;
+  }
+  return 0;
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-    const form = document.getElementById("grading-form");
-    form.reset(); // Reset the form on page load
+  const form = document.getElementById("grading-form");
+  form.reset(); // Reset the form on page load
 
-    document.getElementById("calculate-grade").addEventListener("click", function () {
-        let totalScore = 0;
-        let metCriteria = [];
+  document
+    .getElementById("calculate-grade")
+    .addEventListener("click", function () {
+      let totalScore = 0;
+      let metCriteria = [];
 
-        const criteriaList = [
-            'serverSideAPIs', 'clientSideStorage', 'noJSAlerts', 'nonBootstrapCSS', 'interactiveApp',
-            'uniqueIdea', 'clearIdea',
-            'liveURL', 'githubURL',
-            'uniqueRepoName', 'fileStructure', 'namingConventions', 'commitMessages', 'qualityReadme',
-            'collab',
-        ];
+      const criteriaList = [
+        "serverSideAPIs",
+        "clientSideStorage",
+        "noJSAlerts",
+        "nonBootstrapCSS",
+        "interactiveApp",
+        "uniqueIdea",
+        "clearIdea",
+        "liveURL",
+        "githubURL",
+        "uniqueRepoName",
+        "fileStructure",
+        "namingConventions",
+        "commitMessages",
+        "qualityReadme",
+        "collab",
+        "userExperience",
+        "userInterface",
+        "mockup",
+        "presentation",
+        "presentationPart"
+      ];
 
-        for (const criterion of criteriaList) {
-            const score = getRadioButtonValue(form, criterion);
-            totalScore += score;
-            if (score > 0) {
-                metCriteria.push(criterion);
-            }
+      for (const criterion of criteriaList) {
+        const score = getRadioButtonValue(form, criterion);
+        totalScore += score;
+        if (score > 0) {
+          metCriteria.push(criterion);
         }
+      }
 
-        const metCriteriaTextarea = document.getElementById("met-criteria");
-        metCriteriaTextarea.value = metCriteria.join("\n");
+      const metCriteriaTextarea = document.getElementById("met-criteria");
+      metCriteriaTextarea.value = metCriteria.join("\n");
 
-        // Calculate the percentage score
-        const percentageScore = (totalScore / 100) * 100;
+      // Calculate the percentage score
+      const percentageScore = (totalScore / 100) * 100;
 
-        // Display the result in the "grade-result" div
-        const gradeResult = document.getElementById("grade-result");
-        gradeResult.textContent = `Total Score: ${totalScore} | Percentage: ${percentageScore.toFixed(2)}%`;
+      // Display the result in the "grade-result" div
+      const gradeResult = document.getElementById("grade-result");
+      gradeResult.textContent = `Total Score: ${totalScore} | Percentage: ${percentageScore.toFixed(
+        2
+      )}%`;
     });
 });
