851aedf574239e2e58f574e62a6db5329632bf97
Brandon Ashby <bash7325@gmail.com>
2023-04-11 23:23:53 -0600

    fixed missing default checked value, values reset on page load/refresh

--- script.js
+++ script.js
@@ -8,34 +8,38 @@ function getRadioButtonValue(form, radioButtonGroupName) {
     return 0;
 }
 
-document.getElementById("calculate-grade").addEventListener("click", function () {
+document.addEventListener("DOMContentLoaded", function () {
     const form = document.getElementById("grading-form");
-    let totalScore = 0;
-    let metCriteria = [];
+    form.reset(); // Reset the form on page load
 
-    const criteriaList = [
-        'serverSideAPIs', 'clientSideStorage', 'noJSAlerts', 'nonBootstrapCSS', 'interactiveApp',
-        'uniqueIdea', 'clearIdea',
-        'liveURL', 'githubURL',
-        'uniqueRepoName', 'fileStructure', 'namingConventions', 'commitMessages', 'qualityReadme',
-        'collab',
-    ];
+    document.getElementById("calculate-grade").addEventListener("click", function () {
+        let totalScore = 0;
+        let metCriteria = [];
 
-    for (const criterion of criteriaList) {
-        const score = getRadioButtonValue(form, criterion);
-        totalScore += score;
-        if (score > 0) {
-            metCriteria.push(criterion);
+        const criteriaList = [
+            'serverSideAPIs', 'clientSideStorage', 'noJSAlerts', 'nonBootstrapCSS', 'interactiveApp',
+            'uniqueIdea', 'clearIdea',
+            'liveURL', 'githubURL',
+            'uniqueRepoName', 'fileStructure', 'namingConventions', 'commitMessages', 'qualityReadme',
+            'collab',
+        ];
+
+        for (const criterion of criteriaList) {
+            const score = getRadioButtonValue(form, criterion);
+            totalScore += score;
+            if (score > 0) {
+                metCriteria.push(criterion);
+            }
         }
-    }
 
-    const metCriteriaTextarea = document.getElementById("met-criteria");
-    metCriteriaTextarea.value = metCriteria.join("\n");
+        const metCriteriaTextarea = document.getElementById("met-criteria");
+        metCriteriaTextarea.value = metCriteria.join("\n");
 
-    // Calculate the percentage score
-    const percentageScore = (totalScore / 100) * 100;
+        // Calculate the percentage score
+        const percentageScore = (totalScore / 100) * 100;
 
-    // Display the result in the "grade-result" div
-    const gradeResult = document.getElementById("grade-result");
-    gradeResult.textContent = `Total Score: ${totalScore} | Percentage: ${percentageScore.toFixed(2)}%`;
+        // Display the result in the "grade-result" div
+        const gradeResult = document.getElementById("grade-result");
+        gradeResult.textContent = `Total Score: ${totalScore} | Percentage: ${percentageScore.toFixed(2)}%`;
+    });
 });
